c8a2eec062dac86958d3dae580138534687f2e7c
salaarkhan-dev <salaarkhan311@gmail.com>
2020-11-26 21:25:43 +0500

    Created onepage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 // Mobile Menu Show Hide
 
 const showMenu = (toggleId, menuId) => {
-    const toggle = document.getElementById(toggleId),
-        menu = document.getElementById(menuId)
+  const toggle = document.getElementById(toggleId),
+    menu = document.getElementById(menuId);
 
-    if (toggle && menu) {
-        toggle.addEventListener('click', () => {
-            menu.classList.toggle('show')
-        })
-    }
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  /*Active link*/
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
-
-showMenu('nav-toggle', 'nav-menu')
+navLink.forEach((n) => n.addEventListener("click", linkAction));
