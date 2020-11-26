68a63ab80bee36b0a907f499d11796f8c779d972
salaarkhan-dev <salaarkhan311@gmail.com>
2020-11-26 21:33:45 +0500

    Added smooth scroll

--- js/script.js
+++ js/script.js
@@ -26,3 +26,36 @@ function linkAction() {
   navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home-title", {});
+sr.reveal(".btn", { delay: 200 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".home-social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about-img", {});
+sr.reveal(".about-subtitle", { delay: 400 });
+sr.reveal(".about-text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills-subtitle", {});
+sr.reveal(".skills-text", {});
+sr.reveal(".skills-data", { interval: 200 });
+sr.reveal(".skills-img", { delay: 600 });
+
+/*SCROLL BLOG*/
+sr.reveal(".blogs-subtitle", {});
+sr.reveal(".blogs-text", {});
+sr.reveal(".blog-items", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact-input", { interval: 200 });
